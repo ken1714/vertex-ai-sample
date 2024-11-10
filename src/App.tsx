1924c84db73d69feb01f6f0e5048611a568ceb70
ken1714 <96936578+ken1714@users.noreply.github.com>
2024-11-10 12:05:46 +0900

    [feat] LLMに入力するコンポーネント実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import './App.css';
+
+import SendIcon from '@mui/icons-material/Send';
+import {
+  Box,
+  IconButton,
+  InputAdornment,
+  InputBase,
+  Paper,
+} from '@mui/material';
+import { useState } from 'react';
+
+
+const App = () => {
+  const [inputValue, setInputValue] = useState<string>();
+  const [responseValue, setResponseValue] = useState<string>();
+
+  const sendInput = () => {
+    setResponseValue(inputValue);
+    setInputValue('');
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      if (event.shiftKey) {
+        setInputValue(inputValue + '\n');
+      } else {
+        sendInput();
+      }
+    }
+  };
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    sendInput();
+    event.preventDefault();
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Box
+        sx={{
+          p: '2px 4px',
+          width: '100%'
+        }}
+      >
+        <Paper
+          component="form"
+          onSubmit={handleSubmit}
+        >
+          <InputBase
+            fullWidth
+            multiline
+            value={inputValue}
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="LLMに聞く"
+            inputProps={{ 'aria-label': 'ask-llm' }}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton type="submit">
+                  <SendIcon />
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </Paper>
+        <Paper>
+          {responseValue}
+        </Paper>
+      </Box>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
